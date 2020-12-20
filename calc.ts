c7f26b1a9a81c37731fbc60851f15fde28b93490
nipun <nipung19@gmail.com>
2020-12-20 22:29:40 +0530

    deno bootcamp

---
+++ calc.ts
@@ -0,0 +1,9 @@
+/**
+ * Adds no1 and no2.
+ * @param {number} no1
+ * @param {number} no2
+ * @returns {number} Sum of no1 and no2
+ */
+export function add(no1: number, no2: number): number {
+    return no1 + no2;
+}
